1d2677bb871b8ecc437b84dc92ae665b32767238
Sai Krishnna <nallurisaikrishna37@gmail.com>
2023-08-14 16:52:27 +0530

    added vendor react components

---
+++ frontend/src/components/vendor/EachProposal.js
@@ -0,0 +1,59 @@
+import editicon from '../../Images/pencil-edit-button.svg';
+import deleteicon from '../../Images/bin.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { deleteVendorProposal } from '../../utils/utils.api';
+import { useAppContext } from '../../contexts/ContextProvider';
+
+export default function EachProposal({ vendorProposals, onDeleteFunc, setCreatePage, setEdit }) {
+    const { userDetails } = useAppContext();
+    return <div className="singleProposal">
+        <div className="eventName">{vendorProposals.eventName}</div>
+        <div className="eventDesc">{vendorProposals.description}</div>
+        <div className="detailsContainer">
+            <div className="details">
+                <div>Event Type</div>
+                <section>{vendorProposals.eventType}</section>
+            </div>
+            <div className="details">
+                <div>Proposal Type</div>
+                <section>{vendorProposals.proposaltype}</section>
+            </div>
+            <div className="details">
+                <div>From Date</div>
+                <section>{vendorProposals.from}</section>
+            </div>
+            <div className="details">
+                <div>To Date</div>
+                <section>{vendorProposals.to}</section>
+            </div>
+            <div className="details">
+                <div>Budget</div>
+                <section>{vendorProposals.budget}</section>
+            </div>
+            <div className="optionsContainer">
+                <img src={editicon} alt="Edit" onClick={() => {
+                    setEdit(vendorProposals);
+                    setCreatePage(true);
+                }} />
+                <section onClick={() => {
+                    deleteVendorProposal(vendorProposals._id, userDetails.token).then(res => {
+                        if (res.status === "Success") {
+                            toast.success("Deleted Successfully", {
+                                position: 'top-right'
+                            })
+                            onDeleteFunc(vendorProposals._id);
+                        } else {
+                            toast.error(res.message, {
+                                position: 'top-right'
+                            })
+                        }
+                    })
+                }}>
+                    <img src={deleteicon} alt="Delete" />
+                </section>
+            </div>
+        </div>
+        <ToastContainer />
+    </div>
+}
---
+++ frontend/src/components/vendor/VendorProposals.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import search from '../../Images/search.svg';
+import { useAppContext } from '../../contexts/ContextProvider';
+import { getVendorSpecificProposals } from '../../utils/utils.api';
+import EachProposal from './EachProposal';
+import Createproposal from './Createproposal';
+function VendorProposals() {
+  const { userDetails } = useAppContext();
+  const [proposals, setProposals] = useState([]);
+  const [createPage, setCreatePage] = useState(false);
+  const [edit , setEdit] = useState("");
+  useEffect(() => {
+    getVendorSpecificProposals(userDetails.user._id, userDetails.token).then(res => {
+      setProposals(res.data.reverse());
+    })
+  }, [])
+  return <div className='proposalsHolder'>
+    <span>Proposals</span>
+    <img src={search} alt='Search' />
+    <input placeholder='Search Projects' />
+    <button onClick={() => (
+      setCreatePage(true)
+    )}>
+      CREATE
+    </button>
+    <div>
+      {
+        proposals.length === 0 ? <div>No Proposals to Show</div> :
+          proposals.map((data) => (
+            <EachProposal setEdit ={setEdit} setCreatePage={setCreatePage} vendorProposals={data} key={data._id} onDeleteFunc={(id) => {
+              setProposals(proposals => proposals.filter(data => data._id !== id))
+            }}
+            />
+          ))
+      }
+    </div>
+    {createPage ? <Createproposal edit={edit} setEdit = {setEdit} setProposals = {setProposals} proposals = {proposals} setCreate={setCreatePage} /> : ""}
+  </div>
+}
+
+export default VendorProposals
